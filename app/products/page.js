783e639c8aa53a35029d26c9638a4dca59f9eb32
Winshuka Kannangara <vinshukakannangara@gmail.com>
2025-09-09 00:20:32 +0530

    added necessary imports

--- app/products/page.js
+++ app/products/page.js
@@ -3,6 +3,7 @@
 import { useState } from "react";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import Image from "next/image";
 
 export default function Products() {
   const [selectedCategory, setSelectedCategory] = useState("all");
